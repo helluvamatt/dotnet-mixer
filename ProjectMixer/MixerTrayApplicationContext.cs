847e4c497180e09a50891bdbec3d6f8ec83d59d4
Matt Schneeberger <helluvamatt@gmail.com>
2015-05-17 02:22:24 -0500

    Initial commit / project skeleton

---
+++ ProjectMixer/MixerTrayApplicationContext.cs
@@ -0,0 +1,65 @@
+﻿using Common.UI.Hotkeys;
+using ProjectMixer.Data;
+using ProjectMixer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMixer
+{
+	class MixerTrayApplicationContext : Common.TrayApplication.TrayApplicationContext<SettingsObject>
+	{
+		private HotkeyManager _HotkeyManager;
+
+		protected override void OnInitializeContext()
+		{
+			_HotkeyManager = new HotkeyManager();
+			// TODO
+		}
+
+		protected override Common.TrayApplication.OptionsForm BuildOptionsForm()
+		{
+			return new MixerOptionsForm();
+		}
+
+		protected override void BuildContextMenu()
+		{
+			// TODO
+		}
+
+		protected override Icon ApplicationIcon
+		{
+			get
+			{
+				return new Icon(GetType(), "appicon.ico");
+			}
+		}
+
+		protected override string ApplicationName
+		{
+			get
+			{
+				return Resources.ApplicationName;
+			}
+		}
+
+		protected override string AppDataPath
+		{
+			get
+			{
+				return Path.GetDirectoryName(Path.GetFullPath(Uri.UnescapeDataString(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath)));
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_HotkeyManager.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ ProjectMixer/Program.cs
@@ -0,0 +1,66 @@
+﻿using Common.TrayApplication;
+using log4net.Config;
+using Mono.Options;
+using ProjectMixer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectMixer
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// Check if this is the only instance of this application
+			if (!SingleInstance.Start())
+			{
+				// This is not the only instance, exit
+				Console.WriteLine(Resources.AnotherInstance);
+				return;
+			}
+
+			// Configure logging
+			BasicConfigurator.Configure();
+
+			// Create the TrayApplicationContext - the controller of the entire application
+			MixerTrayApplicationContext ctxt = new MixerTrayApplicationContext();
+			ctxt.Logger.Info("Initializing...");
+
+			// Options
+			OptionSet options = new OptionSet() {
+				{ "startup", "Do not show the options form.", v => ctxt.ShowOptionsForm = v == null }
+			};
+			options.Parse(args);
+
+			// Initialize the application
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			ctxt.InitializeContext();
+
+			// Run the application
+			ctxt.Logger.Info("Initialized. Starting main loop...");
+			try
+			{
+				Application.Run(ctxt);
+			}
+			catch (Exception ex)
+			{
+				ctxt.Logger.Error("Unexpected exception:", ex);
+				MessageBox.Show(ex.Message, Resources.ProgramTerminated, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			// Log the shutdown
+			ctxt.Logger.Info("Exiting...");
+
+			// all finished so release the mutex
+			SingleInstance.Stop();
+		}
+	}
+}
